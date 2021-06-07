2a7d49116c9f8c2cba0a7c5f407fc239c494e036
Tianon Gravi <admwiggin@gmail.com>
2021-06-06 20:17:22 -0700

    Switch from GPL to Apache-2.0
    Closes https://github.com/tianon/gosu/issues/83 (see that thread for contributor approvals/discussion)

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ Usage: {{ .Self }} user-spec command [args]
        {{ .Self }} 1000:1 id
 
 {{ .Self }} version: {{ .Version }}
-{{ .Self }} license: GPL-3 (full text at https://github.com/crazy-max/yasu)
+{{ .Self }} license: Apache-2.0 (full text at https://github.com/crazy-max/yasu)
 `))
 	var b bytes.Buffer
 	template.Must(t, t.Execute(&b, struct {
